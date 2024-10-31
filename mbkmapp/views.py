3b63a3b59d1c924edbcccbc41c7cdb1980c8ecef
Fardhan Erfandyar <fardh.er@gmail.com>
2024-11-01 00:08:16 +0700

    preparation

--- mbkmapp/views.py
+++ mbkmapp/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 
 def landing(request):
-    render(request, "landing.html")
+    return render(request,"landing.html")
+
+def landing(request):
+    return render(request,"landing.html")
